71e2facc288e41256478eccea34ec4d46f38768e
Dave Fernandes <davef@mintleafsoftware.com>
2019-06-29 00:15:18 -0400

    Update for toolchain v0.4.0

--- ECG/ConvModel.swift
+++ ECG/ConvModel.swift
@@ -22,7 +22,7 @@ public struct ConvUnit<Scalar: TensorFlowFloatingPoint> : Layer {
     }
     
     @differentiable
-    public func call(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
+    public func callAsFunction(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
         var tmp = conv2(conv1(input))
         tmp = pool(relu(tmp + input))
         return tmp
@@ -31,35 +31,34 @@ public struct ConvUnit<Scalar: TensorFlowFloatingPoint> : Layer {
 
 public struct ConvModel : Layer {
     var conv1: Conv1D<Float>
-    var unit1: ConvUnit<Float>
-    var unit2: ConvUnit<Float>
-    var unit3: ConvUnit<Float>
-    var unit4: ConvUnit<Float>
-    var unit5: ConvUnit<Float>
+    var convUnit = [ConvUnit<Float>]()
     var dense1: Dense<Float>
     var dense2: Dense<Float>
     var dense3: Dense<Float>
     
+    @noDerivative let convUnitCount = 5
+    
     public init() {
         conv1 = Conv1D<Float>(filterShape: (5, 1, 32), stride: 1, padding: .same)
-        unit1 = ConvUnit<Float>(kernelSize: 5, channels: 32)
-        unit2 = ConvUnit<Float>(kernelSize: 5, channels: 32)
-        unit3 = ConvUnit<Float>(kernelSize: 5, channels: 32)
-        unit4 = ConvUnit<Float>(kernelSize: 5, channels: 32)
-        unit5 = ConvUnit<Float>(kernelSize: 5, channels: 32)
+        for _ in 0..<convUnitCount {
+            convUnit.append(ConvUnit<Float>(kernelSize: 5, channels: 32))
+        }
         dense1 = Dense<Float>(inputSize: 64, outputSize: 32, activation: relu)
         dense2 = Dense<Float>(inputSize: 32, outputSize: 32, activation: relu)
         dense3 = Dense<Float>(inputSize: 32, outputSize: 5)
     }
     
     @differentiable
-    public func call(_ input: Tensor<Float>) -> Tensor<Float> {
+    public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
         var tmp = conv1(input.expandingShape(at: 2))
-        tmp = unit2(unit1(tmp))
-        tmp = unit4(unit3(tmp))
-        tmp = unit5(tmp).reshaped(to: [-1, 64])
-        tmp = dense2(dense1(tmp))
-        tmp = dense3(tmp)
+        
+        tmp = tmp.sequenced(through: convUnit[0], convUnit[1], convUnit[2], convUnit[3], convUnit[4])
+//        for i in 0..<convUnitCount {
+//            let unit = convUnit[i]
+//            tmp = unit(tmp)
+//        }
+        tmp = tmp.reshaped(to: [-1, 64])
+        tmp = tmp.sequenced(through: dense1, dense2, dense3)
         return tmp
     }
     
