365bddf3fbb175452887d4dde3bd2bc823ffc05a
Dave Fernandes <davef@mintleafsoftware.com>
2019-07-20 23:57:40 -0400

    Show crash in main.swift

--- ECG/main.swift
+++ ECG/main.swift
@@ -1,73 +1,80 @@
-//
-//  main.swift
-//  ECG
-//
-//  Created by Dave Fernandes on 2019-03-04.
-//  Copyright © 2019 MintLeaf Software Inc. All rights reserved.
-//
-
 import TensorFlow
-import Python
-import Foundation
 
-let batchSize: Int = 200
-let maxEpochs: Int = 2
+// MARK: - Input data
+struct Example: TensorGroup {
+    var labels: Tensor<Int32>
+    var series: Tensor<Float>
+}
+let example = Example(labels: Tensor<Int32>(ones: [1]), series: Tensor<Float>(ones: [1, 187]))
 
-let (trainDataset, testDataset) = loadDatasets()
-let testBatches = testDataset.batched(1000)
-
-var model = ECGModel()
-let optimizer = Adam(for: model, learningRate: 0.001, decay: 0)
-
-// Training loop
-for epoch in 1...maxEpochs {
-    print("Epoch \(epoch), training...")
+// MARK: - Model definition
+public struct ConvUnit<Scalar: TensorFlowFloatingPoint> : Layer {
+    var conv1: Conv1D<Scalar>
+    var conv2: Conv1D<Scalar>
+    var pool: MaxPool1D<Scalar>
     
-    var trainingLossSum: Float = 0
-    var trainingBatchCount = 0
-    let trainingShuffled = trainDataset.shuffled(sampleCount: 500000, randomSeed: Int64(epoch))
-    let t0 = Date()
+    public init(kernelSize: Int, channels: Int) {
+        conv1 = Conv1D<Scalar>(filterShape: (kernelSize, channels, channels), padding: .same, activation: relu)
+        conv2 = Conv1D<Scalar>(filterShape: (kernelSize, channels, channels), padding: .same)
+        pool = MaxPool1D<Scalar>(poolSize: kernelSize, stride: 2, padding: .valid)
+    }
     
-    // Loop over mini-batches in training set
-    for batch in trainingShuffled.batched(batchSize) {
-        let gradients = gradient(at: model) {
-            (model: ECGModel) -> Tensor<Float> in
-            
-            let thisLoss = loss(model: model, examples: batch)
-            trainingLossSum += thisLoss.scalarized()
-            trainingBatchCount += 1
-            return thisLoss
+    @differentiable
+    public func callAsFunction(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
+        var tmp = input.sequenced(through: conv1, conv2)
+        tmp = pool(relu(tmp + input))
+        return tmp
+    }
+}
+
+public struct ConvModel : Layer {
+    var conv1: Conv1D<Float>
+    var convUnit = [ConvUnit<Float>]()
+    var dense1: Dense<Float>
+    var dense2: Dense<Float>
+    
+    @noDerivative let convUnitCount = 5
+    
+    public init() {
+        conv1 = Conv1D<Float>(filterShape: (5, 1, 32), stride: 1, padding: .same)
+        for _ in 0..<convUnitCount {
+            convUnit.append(ConvUnit<Float>(kernelSize: 5, channels: 32))
         }
-        optimizer.update(&model.allDifferentiableVariables, along: gradients)
+        dense1 = Dense<Float>(inputSize: 64, outputSize: 32, activation: relu)
+        dense2 = Dense<Float>(inputSize: 32, outputSize: 5)
     }
     
-    let t1 = Date()
-    print("  training loss: \(trainingLossSum / Float(trainingBatchCount))  step: \(trainingBatchCount * epoch) (\(t1.timeIntervalSince(t0)) sec)")
-    
-    var testLossSum: Float = 0
-    var testBatchCount = 0
-    
-    // Loop over test set
-    for batch in testBatches {
-        testLossSum += loss(model: model, examples: batch).scalarized()
-        testBatchCount += 1
+    @differentiable
+    public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
+        var tmp = conv1(input.expandingShape(at: 2))
+        
+        // Crashes when using a for-loop
+        for i in 0..<convUnitCount {
+            let unit = convUnit[i]
+            tmp = unit(tmp)
+        }
+        
+        // Does not crash when using sequenced(through:) instead
+//        tmp = tmp.sequenced(through: convUnit[0], convUnit[1], convUnit[2], convUnit[3], convUnit[4])
+        
+        tmp = tmp.reshaped(to: [-1, 64])
+        tmp = tmp.sequenced(through: dense1, dense2)
+        return tmp
     }
-    print("  test loss: \(testLossSum / Float(testBatchCount))")
 }
 
-// Print metrics and confusion matrix
-var yActual = [Int32]()
-var yPredicted = [Int32]()
-
-for batch in testBatches {
-    let labelValues = batch.labels.scalars
-    let predictedValues = model.predictedClasses(for: batch.series).scalars
-    yActual.append(contentsOf: labelValues)
-    yPredicted.append(contentsOf: predictedValues)
+// MARK: - Loss function
+@differentiable(wrt: model)
+func loss(model: ConvModel, examples: Example) -> Tensor<Float> {
+    let logits = model(examples.series)
+    return softmaxCrossEntropy(logits: logits, labels: examples.labels)
 }
 
-let skm = Python.import("sklearn.metrics")
-let report = skm.classification_report(yActual, yPredicted)
-print(report)
-let confusionMatrix = skm.confusion_matrix(yActual, yPredicted)
-print(confusionMatrix)
+// MARK: - Compute a gradient
+var model = ConvModel()
+
+let gradients = gradient(at: model) {
+    (model: ConvModel) -> Tensor<Float> in
+    
+    return loss(model: model, examples: example)
+}
